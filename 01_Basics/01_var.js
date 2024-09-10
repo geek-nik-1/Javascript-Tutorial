377f2183601ffe9ff9b5896b17417de3a48d9869
Nikhil Khot <nikhilkhot976@gmail.com>
2024-09-10 13:06:41 +0000

    data types

---
+++ 01_Basics/01_var.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail = "nikhil@gmail.com"
+var accountPass = "12345"
+accountCity = "Kolhapur"
+let accountState;
+
+accountEmail = "hghg"
+accountPass = "234"
+accountCity = "Pune"
+// accountId = 2
+console.log(accountId);
+console.table([accountEmail, accountPass, accountCity, accountState])
+
+
