32348d576dad7912a23842e970e509fca4cd9283
Nikhil Khot <nikhilkhot976@gmail.com>
2024-09-15 14:19:03 +0000

    Num and Date

---
+++ 01_Basics/date_time.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.getUTCMonth());
+
+let myCreatedDate = new Date(2024, 1, 12)
+console.log(myCreatedDate.toDateString());
+
---
+++ 01_Basics/nums_and_math.js
@@ -0,0 +1,16 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance)
+// console.log(balance.toString())
+
+// console.log(balance.toFixed(2))
+
+const newnum = 123.8966
+
+// console.log(newnum.toPrecision(3))
+
+// Math
+
+console.log(Math.abs(-1))
+console.log(Math.round(4.6));
